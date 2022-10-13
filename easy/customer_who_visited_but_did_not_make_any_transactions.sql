2b3da437361c6109398d40a10a33dd47901e7f03
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-10-13 06:58:37 +0300

    2022-10-13 update: updated "Customer Who Visited but Did Not Make Any Transactions" (#7)

--- easy/customer_who_visited_but_did_not_make_any_transactions.sql
+++ easy/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -3,4 +3,4 @@ select customer_id, count(v.visit_id) as count_no_trans
 from Visits v
 left join Transactions t on v.visit_id = t.visit_id
 where transaction_id is null
-group by customer_id
+group by customer_id;
