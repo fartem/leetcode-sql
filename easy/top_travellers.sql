bb2a3245aa5ca49b62acd15480010aa9b6e8d743
fartem <jaman.smlnsk@gmail.com>
2022-10-25 03:30:06 +0300

    2022-10-25 update: added "Top Travellers"

---
+++ easy/top_travellers.sql
@@ -0,0 +1,5 @@
+/* https://leetcode.com/problems/top-travellers/ */
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance
+from Users u left join Rides r on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name asc;
